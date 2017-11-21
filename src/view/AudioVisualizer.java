26727f0940266fa0f45561c4ead35f290582b8d4
Vincent Mathis <vincemathis94@gmail.com>
2017-11-21 16:01:19 +0100

    Added graphicsContext with rectangles showing spectrum bands.

--- src/view/AudioVisualizer.java
+++ src/view/AudioVisualizer.java
@@ -1,17 +1,17 @@
 package view;
 
-
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.media.AudioSpectrumListener;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.File;
 
 public class AudioVisualizer extends Application implements AudioSpectrumListener {
 
@@ -19,7 +19,9 @@ public class AudioVisualizer extends Application implements AudioSpectrumListene
     // TODO get URI of mp3
     //private static final String AUDIO_URI = new File("mpthreetest.mp3").toURI().toString();
     private static final String AUDIO_URI = System.getProperty("demo.audio.url","http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv");
-
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+    private GraphicsContext gc;
 
     public AudioVisualizer() {
         Media media = new Media(AUDIO_URI);
@@ -38,21 +40,31 @@ public class AudioVisualizer extends Application implements AudioSpectrumListene
         audioPlayer.setCycleCount(Timeline.INDEFINITE);
         audioPlayer.play();
 
-        BorderPane pane = new BorderPane();
-        Scene scene = new Scene(pane, 400, 300);
-
+        Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        stage.setScene(new Scene(root));
         stage.setTitle("Audio Visualizer");
-        stage.setScene(scene);
         stage.show();
     }
 
 
     @Override
     public void spectrumDataUpdate(double timestamp, double duration, float[] magnitudes, float[] phases) {
-        Platform.runLater(() -> {
-            for (float magnitude : magnitudes) {
-                System.out.println(magnitude);
-            }
-        });
+        float[] corrected = new float[magnitudes.length];
+        for (int i = 0; i < magnitudes.length; i++) {
+            corrected[i] = magnitudes[i] - audioPlayer.getAudioSpectrumThreshold();
+        }
+        drawShapes(gc, corrected);
+    }
+
+    private void drawShapes(GraphicsContext gc, float[] mags) {
+        gc.clearRect(0,0, WIDTH, HEIGHT);
+        gc.setFill(Color.BLACK);
+        int width = WIDTH / mags.length;
+        for (int i = 0; i < mags.length; i++) {
+            gc.fillRect(i*width, HEIGHT / 2 - mags[i]*3, width, mags[i]*3*2);
+        }
     }
 }
