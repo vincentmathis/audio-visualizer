5077cffce3890987b862638082ed711ccbd9d1f6
Vincent Mathis <vincemathis94@gmail.com>
2018-01-26 22:11:22 +0100

    circle bars

--- src/controller/VisualizerController.java
+++ src/controller/VisualizerController.java
@@ -38,6 +38,7 @@ public class VisualizerController implements Observer {
 
     private void drawShapes(float bands[]) {
         Platform.runLater(() -> {
+            int radius = 100;
             double width = canvas.getWidth();
             double height = canvas.getHeight();
             double scale = height / 100;
@@ -53,14 +54,14 @@ public class VisualizerController implements Observer {
                 double xDist2 = Math.sin(Math.toRadians((i + 4) * a));
                 double yDist1 = Math.cos(Math.toRadians(i * a));
                 double yDist2 = Math.cos(Math.toRadians((i + 4) * a));
-                double x1 = xCenter + xDist1 * 100;
-                double x2 = xCenter + xDist2 * 100;
-                double x3 = xCenter + xDist2 * 100 * avg;
-                double x4 = xCenter + xDist1 * 100 * avg;
-                double y1 = yCenter - yDist1 * 100;
-                double y2 = yCenter - yDist2 * 100;
-                double y3 = yCenter - yDist2 * 100 * avg;
-                double y4 = yCenter - yDist1 * 100 * avg;
+                double x1 = xCenter + xDist1 * radius;
+                double x2 = xCenter + xDist2 * radius;
+                double x3 = xCenter + xDist2 * (radius + avg);
+                double x4 = xCenter + xDist1 * (radius + avg);
+                double y1 = yCenter - yDist1 * radius;
+                double y2 = yCenter - yDist2 * radius;
+                double y3 = yCenter - yDist2 * (radius + avg);
+                double y4 = yCenter - yDist1 * (radius + avg);
 
                 double xCoords[] = {x1, x2, x3, x4};
                 double yCoords[] = {y1, y2, y3, y4};
